501f8636eae54f73f21a1751c0d0e4263fe9ed2f
xavier pierre <x.pierre@xavier-MBP.attlocal.net>
2024-06-22 16:00:28 -0700

    Add nvim files

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,8 @@
+require("obsidian").setup({
+  workspaces = {
+    {
+      name = "Notes",
+      path = "/Users/x.pierre/Obsidian/Notes",
+    },
+  },
+})
---
+++ nvim/test.py
@@ -0,0 +1,8 @@
+for i in range (10):
+    print (i)
+
+
+def square(x):
+    return x * x
+
+print(square(5))
