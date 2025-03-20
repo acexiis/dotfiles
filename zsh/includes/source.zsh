3b71e25b0a89e8644df1537120c221967b84e1e4
Xavier Pierre <x.pierre@northeastern.edu>
2025-03-19 18:24:29 -0700

    New pro files

---
+++ zsh/includes/source.zsh
@@ -0,0 +1,10 @@
+source $ZSH/oh-my-zsh.sh
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+PATH=/usr/local/bin:$PATH
+#PATH=$HOME/bin:$PATH
+PATH=$PATH:/usr/bin
+
+export PATH=$PATH:/usr/local/mysql/bin
